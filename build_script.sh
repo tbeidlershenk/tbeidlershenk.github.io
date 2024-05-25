a7f10d5f152b85e99364c6a077c04ac49b62ebfe
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2024-05-24 21:52:11 -0400

    Fix transitions plz

---
+++ build_script.sh
@@ -0,0 +1,10 @@
+flutter doctor
+flutter channel stable
+flutter config --enable-web
+flutter upgrade
+flutter pub cache repair
+flutter clean
+flutter pub cache clean
+flutter upgrade
+flutter pub get
+flutter build web --web-renderer html --release --no-tree-shake-icons
